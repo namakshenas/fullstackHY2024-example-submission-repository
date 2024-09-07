2214cea630f5b0071b75888e81893961001d545f
Mohammad Namakshenas <m.n.shenas@gmail.com>
2024-09-07 12:04:38 +0330

    Add Content

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,17 +1,32 @@
 const Header = (props) => {
   return (
-    <>
+    <h1>
     {props.course}
-    </>
+    </h1>
   )
 }
 
+const Content = (props) => {
+  return (
+    <p>
+    {props.name}
+    </p>
+  )
+}
+
+
 const App = () => {
   const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of a component'
 
   return (
     <div>
       <Header course={course} />
+      <Content name={part1}/>
+      <Content name={part2}/>
+      <Content name={part3}/>
     </div>
   )
 }
